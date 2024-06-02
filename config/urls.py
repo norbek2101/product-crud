90e05f17cb36f3dc557318dbd272a9e9acd3d623
norbek <lightnight1223@gmail.com>
2024-06-02 21:40:14 +0500

    add urls and admin

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,42 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Product CRUD API",
+      default_version='v1',
+      description="Product CRUD API description",
+      terms_of_service="https://www.product-crud.uz/policies/terms/",
+      contact=openapi.Contact(email="contact@product-crud.uz"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
 
 urlpatterns = [
+    #admin
     path('admin/', admin.site.urls),
+
+    #accounts
+    path('api/v1/accounts/'),
+
+    #token
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    #swagger
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
